88bb9c1b6fec1c083780fac8be1a42e614a7cb5f
cp <carlos.parram08@gmail.com>
2025-08-26 15:52:48 +0200

    functions.py test model made

---
+++ atlas_roots/functions.py
@@ -0,0 +1,64 @@
+from sentence_transformers import SentenceTransformer, util
+from typing import List
+import pandas as pd
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+def search_places_df(df, query, top_k: int = 3):
+    """
+    Search for places from a DataFrame that match the user's continent, features, and season preferences.
+    """
+
+    #Load data
+    model = SentenceTransformer("sentence-transformers/multi-qa-mpnet-base-dot-v1")
+    query_emb = model.encode(query, convert_to_tensor=True)
+
+
+
+    # 2. User questioning
+
+    # 3. Filter description from dataframe
+    descriptions = df['short_description'].tolist()
+    embeddings = model.encode(descriptions, convert_to_tensor=True)
+
+    # 4. Calculate cosine similarity
+    cos_scores = util.cos_sim(query_emb, embeddings)[0]
+
+    # 5. Top K results
+    top_indices = cos_scores.argsort(descending=True)[:top_k]
+
+
+    # 6. Results
+    results = []
+    for idx in top_indices:
+        row = df.iloc[int(idx)]
+        results.append({
+            "id": row["city"],
+            "name": row["country"],
+            "description": row["short_description"],
+            "score": float(cos_scores[idx])
+        })
+
+    return results
+
+
+
+def get_data(file_path: str) -> pd.DataFrame:
+    """
+    Open data from csv.
+    """
+    # Load the DataFrame from a CSV file (you can change this if you use another source)
+    df = pd.read_csv(file_path)
+
+    # This section has to be according to the dataframe structure
+    df = df[['city', 'country', 'short_description', 'region']]
+
+    return df
+
+
+if __name__ == "__main__":
+    # Example usage
+    df = get_data("/home/scofeels/code/cassiamfs/atlas/.csv/filtered_cities_final.csv")
+    results = search_places_df(df,  "i want quiet town near the sea")
+    for r in results:
+        print(f"City: {r['id']} Country:{r['name']} ({r['score']:.2f}): {r['description']}")
