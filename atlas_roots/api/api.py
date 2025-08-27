5d3cefa1192aa6ed2c837d528ff9c53872c9cb90
cp <carlos.parram08@gmail.com>
2025-08-27 11:14:04 +0200

    building api

--- atlas_roots/api/api.py
+++ atlas_roots/api/api.py
@@ -5,3 +5,13 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {'greeting': 'Hello'}
+
+
+@app.get('/search')
+def search(query: str):
+    return {'search_query': query}
+
+@app.get('/predict')
+def predict(data: dict):
+    return {'prediction': data}
+
