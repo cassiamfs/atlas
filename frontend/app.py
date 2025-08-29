ed184939ba2294efd3a7b2525bf550be5e455eb1
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <magc44214@gmail.com>
2025-08-29 17:54:13 +0200

    Functions commented and sizes adjusted

--- frontend/app.py
+++ frontend/app.py
@@ -5,8 +5,39 @@ from streamlit_lottie import st_lottie
 import time
 #import os
 
-#FUNCTIONS
+#def get_prediction(query, top_k, regions=None, budget=None, seclusion=None):
+    #params = {'query': query, 'top_k': top_k}
+    #if regions:
+        #params['regions'] = regions
+    #if budget:
+        #params['budget'] = budget
+    #if seclusion:
+        #params['seclusion_min'] = seclusion[0]
+        #params['seclusion_max'] = seclusion[1]
 
+    #base_url = 'https://atlas-917734968327.europe-southwest1.run.app'
+
+    #response = requests.get(f'{base_url}/predict_city', params=params)
+    #return response
+
+    #def get_alternatives_by_cluster(cluster_id, exclude_id):
+    #params = {
+        #'cluster_id': cluster_id,
+        #'exclude_id': exclude_id,
+        #'count': 2 #Alternatives amount
+    #}
+    #base_url = 'https://atlas-917734968327.europe-southwest1.run.app'
+
+    #try:
+        #response = requests.get(f'{base_url}/alternatives_by_cluster', params=params)
+        #if response.status_code == 200:
+            #return response.json().get('alternatives', [])
+    #except requests.exceptions.RequestException as e:
+        #st.error(f"Error{e}")
+
+    #return []
+
+#FUNCTIONS
 def get_prediction(query, top_k):
     response = requests.get('https://atlas-917734968327.europe-southwest1.run.app/predict_city', params = {'query': query, 'top_k': top_k})
     return response
@@ -45,13 +76,13 @@ if 'page' not in st.session_state:
 #PÁGINA 1: BIENVENIDA
 if st.session_state.page == 'welcome':
 
-    st.markdown("<h1 style='text-align: center; font-size: 4em;'>Atlas Roots🌍</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; font-size: 8em;'>Atlas Roots🌍</h1>", unsafe_allow_html=True)
     st.markdown("<h2 style='text-align: center; font-size: 2em;'>Find cities based on your description with AI✨</h2>", unsafe_allow_html=True)
     st.markdown("<br><br>", unsafe_allow_html=True)
 
     col1, col2, col3 = st.columns([2, 1, 2])
     with col2:
-        if st.button("Continue 🚀", use_container_width=True):
+        if st.button("Get started!🚀", use_container_width=True):
             st.session_state.page = 'search'
             st.rerun()
 
@@ -96,7 +127,7 @@ elif st.session_state.page == 'search':
     )
     st.markdown("---")
 
-    use_description = st.toggle("Use my own description 🤓", value=True)
+    use_description = st.toggle("Use my own words 🤓", value=True)
     user_query = ""
     if use_description:
         user_query = st.text_area(
@@ -104,25 +135,41 @@ elif st.session_state.page == 'search':
         placeholder="Example: Quiet town near the sea with museums"
     )
 
-    top_k = st.number_input(
-        "Amount of results",
-        min_value=1,
-        max_value=5,
-        value=3,
-        step=1,
-        format="%d"
-    )
+    col1, col2 = st.columns([1, 9])
+    with col1:
+        top_k = st.number_input(
+            "Amount of results",
+            min_value=1,
+            max_value=5,
+            value=3,
+            step=1,
+            format="%d"
+        )
 
     #FIND CITIES
     if st.button("Search🔎") and user_query.strip():
         loading_placeholder = st.empty()
-        lottie_loading_url = "https://assets10.lottiefiles.com/packages/lf20_ydo1amjm.json"
+        lottie_loading_url = "https://assets5.lottiefiles.com/packages/lf20_1pxqjqps.json"
         lottie_loading = load_lottieurl(lottie_loading_url)
 
         with loading_placeholder.container():
             st.markdown("<h3 style='text-align: center;'>🧭Working on it... Get ready for your travel🧳</h3>", unsafe_allow_html=True)
             if lottie_loading:
-                st_lottie(lottie_loading, speed=1, height=140, key="loading")
+                #Columns work as empty margins so the animation is centered
+                col1, col2, col3, col4, col5, col6, col7 = st.columns([1, 1, 1, 1, 1, 1, 1])
+                with col4:
+                    if lottie_loading:
+                        st_lottie(
+                            lottie_loading,
+                            speed=1,
+                            height=150,
+                            key="loading"
+                        )
+
+        #results_api = get_prediction(user_query, top_k, selected_regions, selected_budgets, seclusion_range)
+        #loading_placeholder.empty()
+
+        #st.session_state.results = results_api.json().get('predictions', [])
 
         results_api = get_prediction(user_query, top_k)
         results = results_api.json()['predictions']
@@ -154,41 +201,65 @@ elif st.session_state.page == 'search':
         st.markdown("<h2>Here you have😁🌟</h2>", unsafe_allow_html=True)
 
         #SHOW RESULTS
-        #for r in final_results:
+        #for i, r in enumerate(st.session_state.results):
         for i, r in enumerate (results):
-            #image_path_jpg = f"images/{r['id']}.jpg"
-            #image_path_png = f"images/{r['id']}.png"
+            #col1, col2 = st.columns([1, 2]) # Imagen y Texto
 
-            #if os.path.exists(image_path_jpg):
-                #st.image(image_path_jpg, use_column_width=True)
-            #elif os.path.exists(image_path_png):
-                #st.image(image_path_png, use_column_width=True)
-            #MOSTRAR IMAGEN LOCAL
+            #with col1:
+                #image_path = f"images/{r['id']}.jpg"
+                #st.image(image_path, use_column_width=True)
 
-            #st.markdown(f"### 📍 {r['city']}")
+            #with col2:
             st.markdown(f"""
             <div style='animation: fadeIn 1s ease-in-out {i*0.5}s forwards; opacity:0;'>
                 <h3> ### 📍 {r['id']}</h3>
                 <hr>
             </div>
             """, unsafe_allow_html=True)
-            #st.markdown(f"🌐 Country: {r['country']}")
-            #st.markdown(f"📝 About it: {r['short_description']}")
-            #st.markdown(f"👤 Seclusion: {'👤' * r['seclusion']}")
-            #st.markdown(f"💸 Budget: {budget_map[r['budget_level']]}")
+                #st.markdown(f"🌐 Country: {r['country']}")
+                #st.markdown(f"📝 About it: {r['short_description']}")
+                #st.markdown(f"👤 Seclusion: {r['seclusion']}")
+                #st.markdown(f"💸 Budget: {budget_map[r['budget_level']]}")
+
+                # Alternatives button
+                #if st.button(f"Show similar alternatives", key=f"alt_{r['id']}"):
+                    #alternatives = get_alternatives_by_cluster(r['cluster'], r['id'])
+                    #st.session_state.alternatives[r['id']] = alternatives
+
+                # Show Alternatives
+                #if r['id'] in st.session_state.alternatives:
+                    #with st.expander("✨ Similar Options"):
+                        #alternatives_list = st.session_state.alternatives[r['id']]
+                        #if alternatives_list:
+                            #for alt in alternatives_list:
+                                #st.markdown(f" {alt.get('city', 'N/A')}, {alt.get('country', 'N/A')}: {alt.get('short_description', '')}")
+                        #else:
+                            #st.write("No other similar options were found in our database.")
             st.markdown("---")
-            time.sleep(.7)
+            time.sleep(.4)
 
         #SHOW MAP with the results
+        #map_df = pd.DataFrame([{"lat": r["latitude"], "lon": r["longitude"]} for r in st.session_state.results])
         map_df = pd.DataFrame([{"lat": r["latitude"], "lon": r["longitude"]} for r in results])
         st.map(map_df)
 
-        #lottie_travel_url = "https://assets5.lottiefiles.com/packages/lf20_1pxqjqps.json"
-        #lottie_travel = load_lottieurl(lottie_travel_url)
-        #if lottie_travel:
-            #st_lottie(lottie_travel, speed=1, height=300, key="travel")
+        #https://assets5.lottiefiles.com/packages/lf20_1pxqjqps.json #robot - funciona
+        #https://assets10.lottiefiles.com/packages/lf20_ydo1amjm.json #astronauta - funciona
+
+        lottie_travel_url = "https://assets10.lottiefiles.com/packages/lf20_ydo1amjm.json"
+        lottie_travel = load_lottieurl(lottie_travel_url)
+        col1_b, col2_b, col3_b = st.columns([1, 1, 1])
+        with col2_b:
+            if lottie_travel:
+                st_lottie(
+                    lottie_travel,
+                    speed=1,
+                    height=300,
+                    key="travel"
+                )
+
 
         st.markdown(""" <div style='text-align: center;'>
                     <h2>Enjoy your travel and thanks for trusting us</h2>
-                    <div style='font-size: 2em;'>😎🎒</div>
+                    <div style='font-size: 4em;'>😎🎒</div>
                     </div> """, unsafe_allow_html=True)
